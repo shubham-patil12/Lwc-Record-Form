33eaf351c29aa591230c81b1eaa586d118d999fd
Shubham Patil <154403976+shubham-patil12@users.noreply.github.com>
2024-08-16 15:08:07 +0530

    Add files via upload

---
+++ covidPatients.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import Patient_Details_Object from '@salesforce/schema/Patient_Details__c';
+export default class CovidPatients extends LightningElement {
+    objectApiName = Patient_Details_Object;
+
+}
